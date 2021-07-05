67c0845a0b1c2483ea54db89f7456af1c18f0d29
Alexandre LE CORRE <a.elreco@gmail.com>
2021-07-05 18:53:57 +0200

    :fire: padding on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,20 +26,18 @@
         <div class="container relative mx-auto">
           <div class="items-center flex flex-wrap">
             <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-              <div class="pr-12">
-                <h1 class="text-white font-semibold text-5xl">
-                  Welcome to <span class="text-yellow-500">WhatsNext</span>.
-                </h1>
-                <p class="mt-4 text-lg text-gray-300">
-                  Hello Mark, Whatsnext is an app which allowing you to find
-                  easily partner restaurants around your hotel.
-                  <br /><strong>
-                    Note that it's a
-                    <span class="text-yellow-500">preview version</span> of the
-                    final product. <br />Testing purpose only.</strong
-                  >
-                </p>
-              </div>
+              <h1 class="text-white font-semibold text-5xl">
+                Welcome to <span class="text-yellow-500">WhatsNext</span>.
+              </h1>
+              <p class="mt-4 text-lg text-gray-300">
+                Hello Mark, Whatsnext is an app which allowing you to find
+                easily partner restaurants around your hotel.
+                <br /><strong>
+                  Note that it's a
+                  <span class="text-yellow-500">preview version</span> of the
+                  final product. <br />Testing purpose only.</strong
+                >
+              </p>
             </div>
           </div>
         </div>
